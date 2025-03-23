453b89d3f52f31b07ac61a31063a26356a15285a
Jadj <154758578+Jadj22@users.noreply.github.com>
2025-03-23 16:33:51 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig = {
     images: {
         domains: ["leparle.com"], // Ajoute le domaine ici
     },
+      eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 module.exports = nextConfig;
